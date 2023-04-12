554c20e4e06ce020efc0d1344ef04f643fc1501a
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2023-04-12 18:04:31 -0300

    Adicionando aula7

--- Basico e POO/aluno/henrique/maquinalocal/aula6/Argumentos.java
+++ Basico e POO/aluno/henrique/maquinalocal/aula6/Argumentos.java
@@ -3,6 +3,6 @@ package aluno.henrique.maquinalocal.aula6;
 public class Argumentos {
     public static void main(String[] args) {
         //Não Funcionou.
-        System.out.println("Você digitou " + args[0]);
+        System.out.println("Olá Mundo " + args[0]);
     }
 }
---
+++ Basico e POO/aluno/henrique/maquinalocal/aula7/t1.java
@@ -0,0 +1,4 @@
+package aluno.henrique.maquinalocal.aula7;
+
+public class t1 {
+}
