f894260a9e1b9c190c5a971440eff09f7cd6da2e
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2023-04-12 18:25:54 -0300

    Classe nova trabalhando com erros 02

---
+++ Basico e POO/aluno/henrique/maquinalocal/aula7/TrabalhandoComErros02.java
@@ -0,0 +1,8 @@
+package aluno.henrique.maquinalocal.aula7;
+
+public class TrabalhandoComErros02 {
+    public static void main(String[] args) {
+        int cont = 0;
+        System.out.println("Você digitou: " + cont);
+    }
+}
