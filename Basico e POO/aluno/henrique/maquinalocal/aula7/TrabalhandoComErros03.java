2de1fc91418b68a7c83875d7d9901b1f8ae47ee6
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2023-04-12 18:37:33 -0300

    Atualizando Classe 3 da aula7

---
+++ Basico e POO/aluno/henrique/maquinalocal/aula7/TrabalhandoComErros03.java
@@ -0,0 +1,7 @@
+package aluno.henrique.maquinalocal.aula7;
+
+public class TrabalhandoComErros03 {
+    public static void main(String[] args) {
+        System.out.println("Você digitou: " + 10/2);
+    }
+}
