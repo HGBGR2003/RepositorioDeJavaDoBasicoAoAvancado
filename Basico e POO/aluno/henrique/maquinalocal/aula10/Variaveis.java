9ecd0934afdaf13641b6050a9d398656ef21eff2
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2023-04-13 15:01:36 -0300

    Nova aula

--- Basico e POO/aluno/henrique/maquinalocal/aula10/Variaveis.java
+++ Basico e POO/aluno/henrique/maquinalocal/aula10/Variaveis.java
@@ -2,13 +2,14 @@ package aluno.henrique.maquinalocal.aula10;
 
 public class Variaveis {
     public static void main(String[] args) {
-        int idade = 20;
+        int idade;
         String nome = "Henrique";
         String nomeDoMeuCachorro = "toto";
         idade = 25;
 
         System.out.println("Idade = " + idade);
         System.out.println("Nome = " + nome);
+        System.out.println(nomeDoMeuCachorro);
 
     }
 }
