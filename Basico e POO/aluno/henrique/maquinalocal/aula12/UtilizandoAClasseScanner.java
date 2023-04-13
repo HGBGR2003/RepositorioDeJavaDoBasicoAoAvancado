9db6a5ee8a2e2dcceb8b85f563ef7b19edc8d946
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2023-04-13 16:15:50 -0300

    Atualizando o programa da aula12

--- Basico e POO/aluno/henrique/maquinalocal/aula12/UtilizandoAClasseScanner.java
+++ Basico e POO/aluno/henrique/maquinalocal/aula12/UtilizandoAClasseScanner.java
@@ -1,7 +1,17 @@
 package aluno.henrique.maquinalocal.aula12;
-
+import java.util.Scanner;
 public class UtilizandoAClasseScanner {
     public static void main(String[] args) {
-        
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Digite seu nome completo:");
+        String nomeCompleto = scan.nextLine();
+        System.out.println("O seu nome completo é " +nomeCompleto);
+
+        //Usando o Next
+        System.out.println("Digite seu primeiro nome ?");
+        String primeiroNome = scan.next();
+        System.out.println("O seu primeiro nome é " + primeiroNome);
+
     }
 }
