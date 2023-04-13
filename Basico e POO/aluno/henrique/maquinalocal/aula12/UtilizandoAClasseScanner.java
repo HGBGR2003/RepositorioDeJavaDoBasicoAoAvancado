d8ffc805046b8e26be587b05ad23b3e75d438203
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2023-04-13 16:38:35 -0300

    Exercicio Feito do exemplo

--- Basico e POO/aluno/henrique/maquinalocal/aula12/UtilizandoAClasseScanner.java
+++ Basico e POO/aluno/henrique/maquinalocal/aula12/UtilizandoAClasseScanner.java
@@ -4,14 +4,21 @@ public class UtilizandoAClasseScanner {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        System.out.println("Digite seu nome completo:");
-        String nomeCompleto = scan.nextLine();
-        System.out.println("O seu nome completo é " +nomeCompleto);
 
-        //Usando o Next
-        System.out.println("Digite seu primeiro nome ?");
+        System.out.println("Digite seu primeiro nome, idade, quantidade de filhos, altura e se possui bichinhos?");
         String primeiroNome = scan.next();
-        System.out.println("O seu primeiro nome é " + primeiroNome);
+        int idade = scan.nextInt();
+        byte quantidadeDeFilhos = scan.nextByte();
+        float altura = scan.nextFloat();
+        boolean temPet = scan.nextBoolean();
+
+        System.out.println("-------------------------------------------------------------------------------------");
+        System.out.println("Você digitou os seguintes valores: ");
+        System.out.println("Primeiro Nome: " + primeiroNome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Quantidade de Filhos: " + quantidadeDeFilhos);
+        System.out.println("Altura: " + altura);
+        System.out.println("Tem algum pet em casa? " + temPet);
 
     }
 }
