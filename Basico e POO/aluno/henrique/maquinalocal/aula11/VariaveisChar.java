18ff6898a7bbd0f6a3abdf3d7af4dbc1a1b33024
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2023-04-13 15:31:47 -0300

    Trabalhando com booleano

--- Basico e POO/aluno/henrique/maquinalocal/aula11/VariaveisChar.java
+++ Basico e POO/aluno/henrique/maquinalocal/aula11/VariaveisChar.java
@@ -2,6 +2,15 @@ package aluno.henrique.maquinalocal.aula11;
 
 public class VariaveisChar {
     public static void main(String[] args) {
-        
+        /*
+        char o = 'o';
+        char i = 'i';*/
+
+        char o = 111;
+        char i = 105;
+        char interrogacao = '?';
+
+        System.out.println(" " + o+i+interrogacao);
+
     }
 }
