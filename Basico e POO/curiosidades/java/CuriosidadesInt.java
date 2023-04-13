766313003aed501f5f55cfcb7e0e845412353527
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2023-04-13 15:52:40 -0300

    Uma piada

--- Basico e POO/curiosidades/java/CuriosidadesInt.java
+++ Basico e POO/curiosidades/java/CuriosidadesInt.java
@@ -2,10 +2,11 @@ package curiosidades.java;
 
 public class CuriosidadesInt {
     public static void main(String[] args) {
+        //Referente a aula 11.
         int var1;
         int var2;
         var1 = 2147483647;
-        var2 = 100;
+        var2 = 1;
 
         System.out.println(var1 + var2);
 
