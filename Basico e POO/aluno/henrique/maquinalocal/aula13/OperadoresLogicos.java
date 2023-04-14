fa5f4d2628268bf5be5b0f07845eea398aeb274b
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2023-04-14 18:05:30 -0300

    Operadoes Logicos Nova Classe

---
+++ Basico e POO/aluno/henrique/maquinalocal/aula13/OperadoresLogicos.java
@@ -0,0 +1,13 @@
+package aluno.henrique.maquinalocal.aula13;
+
+public class OperadoresLogicos {
+    public static void main(String[] args) {
+
+        int valor1 = 1;
+        int valor2 = 2;
+
+        boolean resultado1 = (valor1 == 1) && (valor2 == 2);
+        System.out.println("valor1 é 1 AND valor2 é 2 - resultado: " + resultado1);
+
+    }
+}
