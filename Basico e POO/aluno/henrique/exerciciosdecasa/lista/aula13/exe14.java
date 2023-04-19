b201162d5525bcb31b1ec77e0d8dc267b86a875c
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2023-04-19 16:16:11 -0300

    Terminado

--- Basico e POO/aluno/henrique/exerciciosdecasa/lista/aula13/exe14.java
+++ Basico e POO/aluno/henrique/exerciciosdecasa/lista/aula13/exe14.java
@@ -1,7 +1,30 @@
 package aluno.henrique.exerciciosdecasa.lista.aula13;
 
+import java.util.Scanner;
+
 public class exe14 {
     public static void main(String[] args) {
-        
+        final double valorDosPeixes = 50.0;
+        final double valorDaMulta = 4.0;
+        double pesoDosPeixes;
+        double excesso;
+        double multa = 0.0;
+        Scanner imprimir = new Scanner(System.in);
+
+        System.out.println("Informe o peso dos peixes");
+        pesoDosPeixes = imprimir.nextDouble();
+        System.out.println("----------------------------------------------------------------------------");
+
+        excesso = pesoDosPeixes - valorDosPeixes;
+
+        if (pesoDosPeixes > valorDosPeixes){
+            multa = excesso * valorDaMulta;
+            System.out.printf("O peso excedente foi de %.2f kg. O valor da multa a pagar é de R$ %.2f.\n", excesso, multa);
+        }else{
+            System.out.println("Não tem multa");
+        }
+
+
+
     }
 }
