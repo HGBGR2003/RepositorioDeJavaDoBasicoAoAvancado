b6e8dee64e828c9c8a308f89d13ba1b5fb087feb
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2023-04-20 17:11:06 -0300

    Terminado toda lista da aula 13

--- Basico e POO/aluno/henrique/exerciciosdecasa/lista/aula13/ex18.java
+++ Basico e POO/aluno/henrique/exerciciosdecasa/lista/aula13/ex18.java
@@ -1,6 +1,8 @@
 package aluno.henrique.exerciciosdecasa.lista.aula13;
 
 import javax.swing.*;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class ex18 {
     public static void main(String[] args) {
@@ -9,9 +11,15 @@ public class ex18 {
         double velocidadeLink = Double.parseDouble(JOptionPane.showInputDialog("Informe o tamanho" +
                 " do link para download ?"));
 
-        double tempoAproximado = tamanhoMb / velocidadeLink;
+        double velocidadeDownload = velocidadeLink / 8;
 
-        System.out.println("O tempo é " + tempoAproximado + " s");
+        double tempoAproximado = (tamanhoMb / velocidadeDownload) / 60;
+
+        BigDecimal tempo = new BigDecimal(tempoAproximado);
+        tempo = tempo.setScale(2, RoundingMode.HALF_UP);
+        double tempoTruncadoEmDois = tempo.doubleValue();
+
+        System.out.println("O tempo aproximado de download é " + tempoTruncadoEmDois + " minutos");
 
     }
 }
