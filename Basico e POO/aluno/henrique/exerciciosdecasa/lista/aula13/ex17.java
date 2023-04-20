e2b473e78551011ffd9c1ba6230f9f6fd27ec009
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2023-04-20 16:49:55 -0300

    Exercicio 17 terminado

--- Basico e POO/aluno/henrique/exerciciosdecasa/lista/aula13/ex17.java
+++ Basico e POO/aluno/henrique/exerciciosdecasa/lista/aula13/ex17.java
@@ -9,6 +9,7 @@ public class ex17 {
         double metrosQuadradosUsuario;
         Scanner sc = new Scanner(System.in);
         double quantidadeTintas;
+        double valorLata;
 
         System.out.println("Informe o tamanho em metros quadrados?");
         metrosQuadradosUsuario = sc.nextDouble();
@@ -16,6 +17,7 @@ public class ex17 {
         System.out.println("----------------------------------------");
 
         //Letra A
+        /*
         quantidadeTintas = metrosQuadradosUsuario / 108;
         BigDecimal valorDecimal = new BigDecimal(quantidadeTintas);
         valorDecimal = valorDecimal.setScale(2, RoundingMode.HALF_UP);
@@ -28,9 +30,40 @@ public class ex17 {
 
         System.out.println("A quantidade de tintas é " + valorFormula);
         System.out.println("----------------------------------------");
-        System.out.println("Resultado é R$ " + valorLata2);
+        System.out.println("Resultado é R$ " + valorLata2);*/
 
         //Letra B
+        /*
+        quantidadeTintas = metrosQuadradosUsuario / 21.6;
+        BigDecimal valorDecimal = new BigDecimal(quantidadeTintas);
+        valorDecimal = valorDecimal.setScale(2, RoundingMode.HALF_UP);
+        double valorFormula = valorDecimal.doubleValue();
+
+        double valorLata = quantidadeTintas * 25.00;
+        BigDecimal valor = new BigDecimal(valorLata);
+        valor = valor.setScale(2, RoundingMode.HALF_UP);
+        double valorLata2 = valor.doubleValue();
+
+        System.out.println("A quantidade de tintas é " + valorFormula);
+        System.out.println("----------------------------------------");
+        System.out.println("Resultado é R$ " + valorLata2);*/
+
+        quantidadeTintas = metrosQuadradosUsuario / 129.6;
+        double acrecimo = quantidadeTintas * 0.1;
+        BigDecimal valorDecimal = new BigDecimal(quantidadeTintas);
+        valorDecimal = valorDecimal.setScale(2, RoundingMode.HALF_UP);
+        double valorFormula = valorDecimal.doubleValue();
+
+        valorLata = quantidadeTintas * 105.00;
+        BigDecimal valorJunto = new BigDecimal(valorLata);
+        valorJunto = valorJunto.setScale(2, RoundingMode.HALF_UP);
+        double valorDaLataTotal = valorJunto.doubleValue();
+
+        System.out.println("A quantidade de tintas é " + valorFormula);
+        System.out.println("----------------------------------------");
+        System.out.println("Acresentado " + acrecimo);
+        System.out.println("-----------------------------------------");
+        System.out.println("Valor total é " + valorDaLataTotal);
 
     }
 }
