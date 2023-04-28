141058eec4f212717ebc2af3ffd46cc5f4727654
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2023-04-28 11:04:43 -0300

    Exercicio Feito

---
+++ Basico e POO/aluno/henrique/exerciciosdecasa/lista/aula15/ex6.java
@@ -0,0 +1,22 @@
+package aluno.henrique.exerciciosdecasa.lista.aula15;
+
+import javax.swing.*;
+
+public class ex6 {
+    public static void main(String[] args) {
+        int numero1,numero2,numero3;
+
+        numero1 = Integer.parseInt(JOptionPane.showInputDialog("Entre com um número inteiro:"));
+        numero2 = Integer.parseInt(JOptionPane.showInputDialog("Entre com o segundo número inteiro:"));
+        numero3 = Integer.parseInt(JOptionPane.showInputDialog("Entre com o terceiro número inteiro:"));
+
+        if (numero1 > numero2){
+            System.out.println("O número maior é este " + numero1);
+        } else if (numero2 > numero1) {
+            System.out.println("O número maior é este " + numero2);
+        }else if (numero3 > numero1 && numero3 > numero2){
+            System.out.println("O número maior é este " + numero3);
+        }
+
+    }
+}
