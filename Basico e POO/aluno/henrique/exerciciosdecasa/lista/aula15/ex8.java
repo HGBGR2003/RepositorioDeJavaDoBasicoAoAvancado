1ca470f92c83020cc294d54625645c41f11e1e5a
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2023-04-28 11:25:32 -0300

    Arrumando

---
+++ Basico e POO/aluno/henrique/exerciciosdecasa/lista/aula15/ex8.java
@@ -0,0 +1,32 @@
+package aluno.henrique.exerciciosdecasa.lista.aula15;
+
+import java.util.Scanner;
+
+public class ex8 {
+    public static void main(String[] args) {
+        double produto1;
+        double produto2;
+        double produto3;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Informe o preço do produto 1:");
+        produto1 = scan.nextDouble();
+        System.out.println("------------------------------");
+        System.out.println("Informe o preço do produto 2:");
+        produto2 = scan.nextDouble();
+        System.out.println("------------------------------");
+        System.out.println("Informe o preço do produto 3:");
+        produto3 = scan.nextDouble();
+        System.out.println("------------------------------");
+
+        if (produto1 < produto2 && produto1 < produto3){
+            System.out.println("Este é o produto 1 mais barato custando R$ " + produto1);
+        } else if (produto2 < produto1 && produto2 < produto3) {
+            System.out.println("Este é o produto 2 mais barato custando R$ " + produto2);
+        } else if (produto3 < produto1 && produto3 < produto2) {
+            System.out.println("Este é o produto 3 mais barato custando R$ " + produto3);
+        }
+
+
+    }
+}
