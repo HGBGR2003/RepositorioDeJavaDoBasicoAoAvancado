2f351214e0864ee025941ba79a896bcb75e8c1ae
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2023-04-28 11:17:48 -0300

    Novo exercicio

--- Basico e POO/aluno/henrique/exerciciosdecasa/lista/aula15/ex7.java
+++ Basico e POO/aluno/henrique/exerciciosdecasa/lista/aula15/ex7.java
@@ -2,7 +2,7 @@ package aluno.henrique.exerciciosdecasa.lista.aula15;
 
 import javax.swing.*;
 
-public class ex6 {
+public class ex7 {
     public static void main(String[] args) {
         int numero1,numero2,numero3;
 
@@ -10,16 +10,33 @@ public class ex6 {
         numero2 = Integer.parseInt(JOptionPane.showInputDialog("Entre com o segundo número inteiro:"));
         numero3 = Integer.parseInt(JOptionPane.showInputDialog("Entre com o terceiro número inteiro:"));
 
+        //Maior
+
         if (numero1 > numero2 && numero1 > numero3){
             System.out.println("O número maior é este " + numero1);
+            System.out.println("---------------------------------");
         }
 
         if (numero2 > numero1 && numero2 > numero3) {
             System.out.println("O número maior é este " + numero2);
+            System.out.println("---------------------------------");
         }
 
         if (numero3 > numero1 && numero3 > numero2){
             System.out.println("O número maior é este " + numero3);
+            System.out.println("---------------------------------");
+        }
+
+        //Menor
+        if (numero1 < numero2 && numero1 < numero3){
+            System.out.println("O número menor é este " + numero1);
+            System.out.println("---------------------------------");
+        } else if (numero2 < numero1 && numero2 < numero3) {
+            System.out.println("O número menor é este " + numero2);
+            System.out.println("---------------------------------");
+        }else if (numero3 < numero1 && numero3 < numero2){
+            System.out.println("O número menor é este " + numero3);
+            System.out.println("---------------------------------");
         }
 
     }
