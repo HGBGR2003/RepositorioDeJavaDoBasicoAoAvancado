875f66bb6443e70bd89e555b1a0a348517ed6c54
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2023-04-29 20:57:40 -0300

    Continua outro dia

---
+++ Basico e POO/aluno/henrique/exerciciosdecasa/lista/aula15/ex15.java
@@ -0,0 +1,27 @@
+package aluno.henrique.exerciciosdecasa.lista.aula15;
+
+import javax.swing.*;
+
+public class ex15 {
+    public static void main(String[] args) {
+        /*
+        Faça um Programa que peça os 3 lados de um triângulo. O programa
+        deverá informar se os valores podem ser um triângulo. Indique, caso
+        os lados formem um triângulo, se o mesmo é: equilátero, isósceles ou
+        escaleno.
+
+        o Dicas:
+        o Três lados formam um triângulo quando a soma de quaisquer
+        dois lados for maior que o terceiro;
+        o Triângulo Equilátero: três lados iguais;
+        o Triângulo Isósceles: quaisquer dois lados iguais;
+        o Triângulo Escaleno: três lados diferentes;
+         */
+        int a;
+        int b;
+        int c;
+
+        JOptionPane.showInputDialog("Entre com o primeiro lado do triangulo: ");
+
+    }
+}
