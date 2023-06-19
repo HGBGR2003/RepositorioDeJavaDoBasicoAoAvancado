52f1b280d97691931164fba3e49b29c338461b33
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2023-06-19 19:13:26 -0300

    Atualizado e terminado

--- Basico e POO/aluno/henrique/exerciciosdecasa/lista/aula15/ex26.java
+++ Basico e POO/aluno/henrique/exerciciosdecasa/lista/aula15/ex26.java
@@ -15,46 +15,52 @@ public class ex26 {
 
         System.out.println("Gasolina ou Alcool (1 OU 2):");
         gasAl = scan.nextInt();
+        System.out.println("-----------------------------------------");
+
 
         switch (gasAl){
             case 1:
                 System.out.println("Entre com a quantidade de litros de gasolina que o cliente pediu:");
                 litrosGasolina = scan.nextDouble();
+                System.out.println("-------------------------------------------------------------------");
                 break;
 
             case 2:
                 System.out.println("Entre com a quantidade de litros de alcool que o cliente pediu:");
                 litrosAlcool = scan.nextDouble();
+                System.out.println("-------------------------------------------------------------------");
                 break;
 
             default:
                 System.out.println("Informação Inválida");
         }
 
-        //Alcool
-        if (litrosAlcool <= 20){
-            descontoAlcool = quantidadeLitroDeAlcoolComprado - 0.03;
-            double totalCompradoAlcool = litrosAlcool * descontoAlcool;
-            System.out.println("De "  + litrosAlcool  + " preço a ser pago de 3 % com o desconto : "  + totalCompradoAlcool);
-        } else if (litrosAlcool > 20) {
-           double descontoAlcool1 = quantidadeLitroDeAlcoolComprado - 0.05;
-           double totalCompradoAlcool1 = litrosAlcool * quantidadeLitroDeAlcoolComprado;
-            System.out.println("De "  + litrosAlcool  + " preço a ser pago de 5 % com o desconto : "  + totalCompradoAlcool1);
-        }
-
-
         //Gasolina
-        if (litrosGasolina < 20){
-            descontoGasolina = quantidadeLitroDeGasolinaComprado - 0.04;
-            double totalCompradoGasolina = descontoGasolina * litrosGasolina;
-            System.out.println(" De "  + litrosGasolina  + " preço a ser pago de 4 % com o desconto : "  + totalCompradoGasolina);
-        } else if (litrosGasolina > 20) {
-            descontoGasolina = litrosGasolina - 0.06;
-            double totalCompradoGasolina1 = descontoGasolina * litrosGasolina;
-            System.out.println(" De "  + litrosGasolina  + " preço a ser pago de 6 % com o desconto : "  + totalCompradoGasolina1);
+        if (litrosGasolina != 0){
+            if (litrosGasolina <= 20){
+                descontoGasolina = quantidadeLitroDeGasolinaComprado - 0.04;
+                double totalCompradoGasolina = descontoGasolina * litrosGasolina;
+                System.out.println("De "  + litrosGasolina  + "L o preço a ser pago é de 4 % com o desconto : R$ "  + totalCompradoGasolina);
+            } else if (litrosGasolina > 20) {
+                descontoGasolina = litrosGasolina - 0.06;
+                double totalCompradoGasolina1 = descontoGasolina * litrosGasolina;
+                System.out.println("De "  + litrosGasolina  + "L o preço a ser pago é de 6 % com o desconto : R$ "  + totalCompradoGasolina1);
+            }
         }
 
 
+        //Alcool
+        if (litrosAlcool != 0){
+            if (litrosAlcool <= 20){
+                descontoAlcool = quantidadeLitroDeAlcoolComprado - 0.03;
+                double totalCompradoAlcool = litrosAlcool * descontoAlcool;
+                System.out.println("De "  + litrosAlcool  + "L o preço a ser pago é de 3 % com o desconto : R$ "  + totalCompradoAlcool);
+            } else if (litrosAlcool > 20) {
+                double descontoAlcool1 = quantidadeLitroDeAlcoolComprado - 0.05;
+                double totalCompradoAlcool1 = litrosAlcool * quantidadeLitroDeAlcoolComprado;
+                System.out.println("De "  + litrosAlcool  + "L o preço a ser pago de 5 % com o desconto : R$ "  + totalCompradoAlcool1);
+            }
+        }
 
 
     }
