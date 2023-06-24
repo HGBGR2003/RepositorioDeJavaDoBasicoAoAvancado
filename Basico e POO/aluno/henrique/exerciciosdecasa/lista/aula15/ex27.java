729bd76005c8057ef2810579147e30ade541cb1f
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2023-06-24 11:19:17 -0300

    Terminado

--- Basico e POO/aluno/henrique/exerciciosdecasa/lista/aula15/ex27.java
+++ Basico e POO/aluno/henrique/exerciciosdecasa/lista/aula15/ex27.java
@@ -22,9 +22,10 @@ public class ex27 {
             totalPreco = quantidadeMorangosKg * 2.50;
             double separacao = Math.floor(totalPreco * 100)/100;
             System.out.println("Total do preço do kilo dos morangos até 5 Kg: R$ " + separacao);
+            System.out.println("----------------------------------------------------------------");
         }
 
-        if (quantidadeMorangosKg > 5 && quantidadeMacasKg <= 8){
+        if (quantidadeMorangosKg > 5){
             double totalPreco2 = quantidadeMorangosKg * 2.20;
             double separacao = Math.floor(totalPreco2 * 100)/100;
             if (totalPreco2 > 25){
@@ -32,14 +33,16 @@ public class ex27 {
             }else{
                 desconto = separacao;
             }
-            System.out.println("Total do preço do kilo dos morangos de Acima de 5 Kg: R$ " + separacao);
+            System.out.println("Total do preço do kilo dos morangos de Acima de 5 Kg ou 8 Kg: R$ " + separacao);
+            System.out.println("-------------------------------------------------------------------------------");
         }
 
         if (quantidadeMacasKg <= 5){
             double totalPrecoMacas = quantidadeMacasKg * 1.80;
             double trucamento = Math.floor(totalPrecoMacas * 100)/100;
             System.out.println("Total do preço do kilo das maçãs até de 5 Kg: R$ " + trucamento);
-        } else if (quantidadeMacasKg > 5 && quantidadeMacasKg <= 8) {
+            System.out.println("---------------------------------------------------------------");
+        } else if (quantidadeMacasKg  > 5) {
             double totalPrecosMacas2 = quantidadeMacasKg * 1.50;
             double truncamento = Math.floor(totalPrecosMacas2 * 100)/100;
             if (totalPrecosMacas2 > 25){
@@ -47,7 +50,7 @@ public class ex27 {
             }else{
                 desconto = truncamento;
             }
-            System.out.println("Total do preço do kilo das maçãs de Acima de 5 Kg: R$ " + truncamento);
+            System.out.println("Total do preço do kilo das maçãs de Acima de 5 ou 8 Kg: R$ " + truncamento);
         }
 
 
