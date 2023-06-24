53fce5b68fdc6973fd4cce49cbd27d4739c382c7
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2023-06-24 15:41:18 -0300

    Fazendo

--- Basico e POO/aluno/henrique/exerciciosdecasa/lista/aula15/ex28.java
+++ Basico e POO/aluno/henrique/exerciciosdecasa/lista/aula15/ex28.java
@@ -16,7 +16,7 @@ public class ex28 {
 
         switch (tipo){
             case "file duplo":
-            double quantidadeDeCarnesKiloFile = Double.parseDouble(JOptionPane.showInputDialog("Entre com a quantidade de carnes de file duplo:"));
+            double quantidadeDeCarnesKiloFile = Double.parseDouble(JOptionPane.showInputDialog("Entre com a quantidade em Kilo de carnes de file duplo:"));
                 if (quantidadeDeCarnesKiloFile <= 5){
                     double totalFile = quantidadeDeCarnesKiloFile * 4.90;
                     tipoDePagamento = JOptionPane.showInputDialog("Entre com o tipo de pagamento: (Cartão tabajara 10 %)");
@@ -26,6 +26,7 @@ public class ex28 {
                         desconto = totalFile;
                     }
                     System.out.println("Total do preço do kilo dos filés até 5 Kg, a quantidade de filés é " + quantidadeDeCarnesKiloFile + " e o tipo do pagamento foi " + tipoDePagamento + " e o valor a ser pago é de R$ : " + desconto);
+
                 } else if (quantidadeDeCarnesKiloFile > 5) {
                     double totalFile2 = quantidadeDeCarnesKiloFile * 5.80;
                     tipoDePagamento = JOptionPane.showInputDialog("Entre com o tipo de pagamento: (Cartão tabajara 10 %)");
@@ -36,8 +37,40 @@ public class ex28 {
                     }
                     System.out.println("Total do preço do kilo dos filés Acima de 5 Kg, a quantidade de filés é " + quantidadeDeCarnesKiloFile + " e o tipo do pagamento foi " + tipoDePagamento + " e o valor a ser pago é de R$ : " + desconto);
                 }
+
+                break;
+
             case "alcatra":
+                double quantidadeDeCarnesAlcatra = Double.parseDouble(JOptionPane.showInputDialog("Entre com a quantidade em Kilo de carnes de alcatra:"));
+                if (quantidadeDeCarnesAlcatra <= 5){
+                    double totalAlcatra = quantidadeDeCarnesAlcatra * 5.90;
+                    tipoDePagamento = JOptionPane.showInputDialog("Entre com o tipo de pagamento: (Cartão tabajara 10 %)");
+                    if (tipoDePagamento == "cartao tabajara"){
+                        desconto = totalAlcatra - 0.1;
+                    }else{
+                        desconto = totalAlcatra;
+                    }
+                    System.out.println("Total do preço do kilo da alcatra até 5 Kg, a quantidade de filés é " + quantidadeDeCarnesAlcatra + " e o tipo do pagamento foi " + tipoDePagamento + " e o valor a ser pago é de R$ : " + desconto);
+                }
+
+                if (quantidadeDeCarnesAlcatra > 5){
+                    double total2 = quantidadeDeCarnesAlcatra * 5.80;
+                    tipoDePagamento = JOptionPane.showInputDialog("Entre com o tipo de pagamento: (Cartão tabajara 10 %)");
+                    if (tipoDePagamento == "cartao tabajara"){
+                        desconto = total2 - 0.1;
+                    }else{
+                        desconto = total2;
+                    }
+                    System.out.println("Total do preço do kilo da alcatra até 5 Kg, a quantidade de filés é " + quantidadeDeCarnesAlcatra + " e o tipo do pagamento foi " + tipoDePagamento + " e o valor a ser pago é de R$ : " + desconto);
+                }
+
+                break;
+
             case "picanha":
+                double quantidadeDeCarnesPicanha = Double.parseDouble(JOptionPane.showInputDialog("Entre com a quantidade em Kilo de carnes de picanha:"));
+                if (quantidadeDeCarnesPicanha <= 5){
+
+                }
         }
     }
 }
