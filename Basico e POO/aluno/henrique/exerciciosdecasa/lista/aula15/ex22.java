52550e9fe9236d2feb3dc0dbbd2e201b8a7c9bc1
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2023-07-03 21:05:30 -0300

    Lista Feita com sucesso

--- Basico e POO/aluno/henrique/exerciciosdecasa/lista/aula15/ex22.java
+++ Basico e POO/aluno/henrique/exerciciosdecasa/lista/aula15/ex22.java
@@ -1,10 +1,43 @@
 package aluno.henrique.exerciciosdecasa.lista.aula15;
 
-import javax.swing.*;
+import java.util.Scanner;
 
 public class ex22 {
     public static void main(String[] args) {
+        Scanner ajudador = new Scanner(System.in);
 
+        System.out.println("Entre com a quantidade (Kg) de morangos:");
+        double qtdMorango = ajudador.nextDouble();
+
+        System.out.println("Entre com a quantidade (Kg) de maças:");
+        double qtdMaca = ajudador.nextDouble();
+
+        double precoKgMorango = 0;
+
+        if (qtdMorango <= 5){
+            precoKgMorango = 2.5;
+        }else{
+            precoKgMorango = 2.2;
+        }
+
+        double precoKgMaca = 0;
+
+        if (qtdMaca <= 5){
+            precoKgMaca = 1.8;
+        }else{
+            precoKgMaca = 1.5;
+        }
+
+        double precoTotalMorango = qtdMorango * precoKgMorango;
+        double precoTotalMaca = qtdMaca * precoKgMaca;
+
+        double precoParcial = precoKgMaca + precoKgMorango;
+        double precoTotal = precoParcial;
+
+        if ((qtdMorango + qtdMaca > 8) || precoParcial > 25){
+            precoTotal = precoParcial -  ((precoParcial) / 100) * 10;
+        }
+        System.out.println("Preco Total = " + precoTotal);
     }
 }
 
