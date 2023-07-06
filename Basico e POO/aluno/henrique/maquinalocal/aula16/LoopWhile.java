ddc1bc7fed71695577de56f7a775e3b607b59ddd
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2023-07-06 15:52:18 -0300

    Concluido

--- Basico e POO/aluno/henrique/maquinalocal/aula16/LoopWhile.java
+++ Basico e POO/aluno/henrique/maquinalocal/aula16/LoopWhile.java
@@ -1,10 +1,7 @@
 package aluno.henrique.maquinalocal.aula16;
 
-import java.util.Scanner;
-
 public class LoopWhile {
     public static void main(String[] args) {
-        Scanner impressora = new Scanner(System.in);
         int i = 1;
         int max = 10;
 
@@ -14,6 +11,14 @@ public class LoopWhile {
             System.out.println("O valor de i: é " + i);
             i++;
         }
+
+        System.out.println(i); //Valor de 11
+
+        do {
+            i++;
+            System.out.println("O valor de i: é " + i);
+        }while (i < 13);
+
         System.out.println(i);
     }
 }
