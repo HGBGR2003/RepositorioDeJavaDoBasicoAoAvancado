230826d713b245d853c61c0e669a7bdc6aea2fc6
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2023-07-06 15:36:23 -0300

    Nova pacote 16

---
+++ Basico e POO/aluno/henrique/maquinalocal/aula16/text1.java
@@ -0,0 +1,9 @@
+package aluno.henrique.maquinalocal.aula16;
+
+import java.util.Scanner;
+
+public class text1 {
+    public static void main(String[] args) {
+        Scanner impressora = new Scanner(System.in);
+    }
+}
