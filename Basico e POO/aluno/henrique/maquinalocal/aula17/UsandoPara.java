64806b176b82afd3989eea818f0c031b19c3d605
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2023-07-06 16:39:17 -0300

    Concluido aula17

--- Basico e POO/aluno/henrique/maquinalocal/aula17/UsandoPara.java
+++ Basico e POO/aluno/henrique/maquinalocal/aula17/UsandoPara.java
@@ -2,7 +2,6 @@ package aluno.henrique.maquinalocal.aula17;
 
 public class UsandoPara {
     public static void main(String[] args) {
-        /*
         for (int i = 0; i < 5; i++){
             System.out.println("i tem valor: " + i);
         }
@@ -13,15 +12,40 @@ public class UsandoPara {
             System.out.println("i tem valor: " + i);
         }
 
+        System.out.println("-------------------------------------");
+
         //Outro jeito (Novo com duas variaveis) <= Ps: Nunca vi isso.
         for (int i = 0, k = 10; i < k; i++, k--){
             System.out.println("i = " + i + "; k = " + k);
-        }*/
-        int count = 0;
-        for (; count < 5 ;){
-            System.out.println("valor de count:");
         }
 
+        System.out.println("-------------------------------------------");
 
+        int count = 0;
+
+        for (; count < 10 ;){
+            System.out.println("Valor de count: " + count);
+            count += 2;
+        }
+
+        System.out.println("----------------------------------");
+
+        for (int cont = 0; cont < 10; cont += 2){
+            System.out.println("Valor de cont: " + cont);
+        }
+
+        System.out.println("------------------------------");
+
+        //Exemplo sem a as chaves e com as chaves.
+        int soma = 0;
+        for (int i = 0; i < 5; soma += i++);
+        System.out.println("O valor da soma é " + soma);
+
+        System.out.println("---------------------------------");
+
+        for (int i =  0; i < 5; i++){
+            System.out.println("i tem valor: " + i);
+            System.out.println("i tem valor: ");
+        }
     }
 }
