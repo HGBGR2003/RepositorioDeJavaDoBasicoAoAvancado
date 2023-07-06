f010b3bee81b84f1184d1e26346c860de5c611e8
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2023-07-06 16:25:12 -0300

    Continuando exemplos aula-17

--- Basico e POO/aluno/henrique/maquinalocal/aula17/UsandoPara.java
+++ Basico e POO/aluno/henrique/maquinalocal/aula17/UsandoPara.java
@@ -2,6 +2,26 @@ package aluno.henrique.maquinalocal.aula17;
 
 public class UsandoPara {
     public static void main(String[] args) {
-        
+        /*
+        for (int i = 0; i < 5; i++){
+            System.out.println("i tem valor: " + i);
+        }
+
+        System.out.println("------------------------------");
+
+        for (int i = 5; i > 0; i--){
+            System.out.println("i tem valor: " + i);
+        }
+
+        //Outro jeito (Novo com duas variaveis) <= Ps: Nunca vi isso.
+        for (int i = 0, k = 10; i < k; i++, k--){
+            System.out.println("i = " + i + "; k = " + k);
+        }*/
+        int count = 0;
+        for (; count < 5 ;){
+            System.out.println("valor de count:");
+        }
+
+
     }
 }
