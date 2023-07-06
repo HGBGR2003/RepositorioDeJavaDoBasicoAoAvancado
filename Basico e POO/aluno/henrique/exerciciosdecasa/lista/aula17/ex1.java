5e9f7fd6ae01af96590f93909fdd99e784c924fb
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2023-07-06 17:47:38 -0300

    Comecou

---
+++ Basico e POO/aluno/henrique/exerciciosdecasa/lista/aula17/ex1.java
@@ -0,0 +1,23 @@
+package aluno.henrique.exerciciosdecasa.lista.aula17;
+
+import java.util.Scanner;
+
+public class ex1 {
+    public static void main(String[] args) {
+        int entradaUsuario;
+        Scanner impressora = new Scanner(System.in);
+
+        System.out.println("Informe uma nota entre 0 e 10 ?");
+        entradaUsuario = impressora.nextInt();
+
+        while (entradaUsuario < 0 || entradaUsuario > 10){
+            System.out.println("Entre de novo com uma nota entre 0 e 10:");
+            entradaUsuario = impressora.nextInt();
+        }
+
+        if (entradaUsuario >= 0 && entradaUsuario <= 10){
+            System.out.println("Informação nos Padrões estabelecidos.");
+        }
+
+    }
+}
