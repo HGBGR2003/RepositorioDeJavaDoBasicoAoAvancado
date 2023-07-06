8c569c58e8900e24cd286b3d93d5caa3a027e3d6
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2023-07-06 18:36:56 -0300

    Exercicio 3 terminado

--- Basico e POO/aluno/henrique/exerciciosdecasa/lista/aula17/ex3.java
+++ Basico e POO/aluno/henrique/exerciciosdecasa/lista/aula17/ex3.java
@@ -10,5 +10,63 @@ public class ex3 {
         double salario;
         String sexoHumano;
         String estadoCivil;
+
+        System.out.println("Informe o seu nome:");
+        nomeUsuario = imp.next();
+
+        System.out.println("--------------------------");
+
+        System.out.println("Entre com sua idade:");
+        idade = imp.nextInt();
+
+        System.out.println("--------------------------");
+
+        System.out.println("Entre com seu sálario:");
+        salario = imp.nextDouble();
+
+        System.out.println("--------------------------");
+
+        System.out.println("Entre com seu sexo (F/M) :");
+        sexoHumano = imp.next();
+
+        System.out.println("--------------------------");
+
+        System.out.println("Entre com seu estado civil:(S/C/V/D)");
+        estadoCivil = imp.next();
+
+        System.out.println("--------------------------");
+
+        while (nomeUsuario.length() > 3){
+            System.out.println("Informe o nome de novo:");
+            nomeUsuario = imp.next();
+            System.out.println("--------------------------");
+        }
+
+        while (idade < 0 || idade > 150){
+            System.out.println("Informe sua idade de novo:");
+            idade = imp.nextInt();
+            System.out.println("--------------------------");
+        }
+
+        while (salario <= 0){
+            System.out.println("Entre com um salario maior:");
+            salario = imp.nextDouble();
+            System.out.println("--------------------------");
+        }
+
+        while (!sexoHumano.equalsIgnoreCase("f") && !sexoHumano.equalsIgnoreCase("m")){
+            System.out.println("Entre com um sexo correto (F/M)");
+            sexoHumano = imp.next();
+            System.out.println("--------------------------");
+        }
+
+        while (!estadoCivil.equalsIgnoreCase("s") && !estadoCivil.equalsIgnoreCase("c")
+        && !estadoCivil.equalsIgnoreCase("v") && !estadoCivil.equalsIgnoreCase("d")){
+            System.out.println("Entre com seu estado civil seguindo essa nomiação: [S/C/V/D] ");
+            estadoCivil = imp.next();
+            System.out.println("--------------------------");
+        }
+
+        System.out.println("Sucesso!");
     }
 }
