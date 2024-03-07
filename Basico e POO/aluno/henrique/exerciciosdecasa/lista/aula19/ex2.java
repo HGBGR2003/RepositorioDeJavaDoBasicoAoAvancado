3f70957a95a547b51856eb9409e3f8f96887dec4
Henrique Gabriel <henriquegb152019@gmail.com>
2024-03-07 15:25:30 -0300

    Feito

--- Basico e POO/aluno/henrique/exerciciosdecasa/lista/aula19/ex2.java
+++ Basico e POO/aluno/henrique/exerciciosdecasa/lista/aula19/ex2.java
@@ -1,7 +1,29 @@
 package aluno.henrique.exerciciosdecasa.lista.aula19;
 
+import javax.swing.*;
+
 public class ex2 {
     public static void main(String[] args) {
+        String nomeUsuario;
+        String senha;
+        boolean resposta;
+
+        nomeUsuario = JOptionPane.showInputDialog(null, "Digite seu nome de usuário");
+        senha = JOptionPane.showInputDialog("Entre com a senha");
+
+        do {
+            if (senha.equals(nomeUsuario)) {
+                JOptionPane.showMessageDialog(null, "Tente de Novo!");
+                nomeUsuario = JOptionPane.showInputDialog(null, "Digite seu nome de usuário");
+                senha = JOptionPane.showInputDialog("Entre com a senha");
+                resposta = true;
+            }
+
+        } while (resposta = false);
+
+        System.out.println("Nome de Usuário: " + nomeUsuario);
+        System.out.println("Sua Senha: " + senha);
+
 
     }
 }
