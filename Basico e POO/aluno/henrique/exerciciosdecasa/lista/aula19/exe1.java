e6bc004be000a006976768bdc53ddf2852c11906
Henrique Gabriel <henriquegb152019@gmail.com>
2024-03-09 13:49:47 -0300

    Feito.

--- Basico e POO/aluno/henrique/exerciciosdecasa/lista/aula19/exe1.java
+++ Basico e POO/aluno/henrique/exerciciosdecasa/lista/aula19/exe1.java
@@ -1,7 +1,20 @@
 package aluno.henrique.exerciciosdecasa.lista.aula19;
 
+import javax.swing.*;
+
 public class exe1 {
     public static void main(String[] args) {
+        int [] vetorA = new int[5];
+        int [] vetorB = new int[5];
+
+        for (int i = 0; i < vetorA.length; i++){
+            int usuarioVetor = Integer.parseInt(JOptionPane.showInputDialog("Entre com um número inteiro:"));
+            vetorA[i] = usuarioVetor;
+            vetorB [i] = vetorA[i];
+            System.out.println( "Vetor A: " + vetorA[i] + " ---- " + " Vetor B: " + vetorB[i]);
+
+        }
+
 
     }
 }
